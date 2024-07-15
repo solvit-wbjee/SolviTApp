80cf21753c1d33d9a28d20e90da31e8604aeb813
ankan321 <adalui260@gmail.com>
2024-07-15 20:25:03 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        punycode: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
