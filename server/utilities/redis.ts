61d7faf99ae2b67d91251e163471c3ec590beb9d
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-15 22:03:02 +0530

    updated

--- server/utilities/redis.ts
+++ server/utilities/redis.ts
@@ -1,26 +1,12 @@
 import { Redis } from 'ioredis';
 require('dotenv').config();
 
-type RedisUrl = string | undefined; // Define a type for Redis URL (can be string or undefined)
-
-const redisUrl: RedisUrl = process.env.REDIS_URL; // Use type annotation
-
-const connectRedis = async (): Promise<Redis> => {
-    if (!redisUrl) {
-        throw new Error('Missing REDIS_URL environment variable');
+const redisClient = () => {
+    if (process.env.REDIS_URL) {
+        console.log(`Redis connected`);
+        return process.env.REDIS_URL;
     }
-
-    const client = new Redis(redisUrl);
-
-    try {
-        await client.connect();
-        console.log('Redis connected successfully');
-        return client;
-    } catch (error) {
-        console.error('Redis connection error:', error);
-        throw error; // Re-throw the error to indicate connection failure
-    }
+    throw new Error('Redis connection failed');
 };
 
-// Export the connection function to be used elsewhere
-export { connectRedis };
+export const redis = new Redis(redisClient());
