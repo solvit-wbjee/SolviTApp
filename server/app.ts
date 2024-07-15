689c49dc31faf99b4a2c22c34cb5909ceee257ce
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-15 11:15:03 +0530

    server conncretd

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,34 @@
-import express from 'express';
+require('dotenv').config()
+
+import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
+import { error } from 'console';
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+
+//cookie-parser
+app.use(cookieParser());
+
+
+//cors=>cors origin resources sharing
+app.use(cors({
+    origin: process.env.ORIGIN
+}))
+
+//testing api
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is Working"
+    })
+})
+
+// Error handling for unknown routes (catch-all)
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.StatusCode = 404 // Set the error status code to 404 (Not Found)
+    next(error); // Pass the error to the error handler middleware
+});
