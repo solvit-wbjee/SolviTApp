d563b41236bfc4e8b29838c69073984ccb263585
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-16 23:49:29 +0530

    updated logout

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -23,6 +23,7 @@ userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
 
+// userRouter.get("/logout", isAutheticated, logoutUser);
 userRouter.get("/logout",  logoutUser);
 
 userRouter.get('/refreshtoken',updateAccessToken)
