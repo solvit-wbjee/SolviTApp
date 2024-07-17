d67e604d0c7ac9bcccf9019d1048fe1ebba6c7f4
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-17 18:58:13 +0530

    accestoken error solved

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -198,6 +198,65 @@ export const logoutUser = CatchAsyncError(
 
 // update access token
 // access token will expire soon (5m) but refresh token  expire (3d)
+// export const updateAccessToken = CatchAsyncError(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const refresh_token = req.cookies.refresh_token as string;
+//       const decoded = jwt.verify(
+//         refresh_token,
+//         process.env.REFRESH_TOKEN as string
+//       ) as JwtPayload;
+
+//       const message = "Could not refresh token";
+//       if (!decoded) {
+//         return next(new ErrorHandler(message, 400));
+//       }
+//       const session = await redis.get(decoded.id as string);
+         
+//       if (!session) {
+//         return next(
+//           new ErrorHandler("Please login for access this resources!", 400)
+//         );
+//       }
+      
+//       const user = JSON.parse(session);
+
+//       const accessToken = jwt.sign(
+//         { id: user._id },
+//         process.env.ACCESS_TOKEN as string,
+//         {
+//           expiresIn: "5m",
+//         }
+//       );
+
+//       const refreshToken = jwt.sign(
+//         { id: user._id },
+//         process.env.REFRESH_TOKEN as string,
+//         {
+//           expiresIn: "3d",
+//         }
+//       );
+
+//       req.user = user;
+
+//       res.cookie("access_token", accessToken, accessTokenOptions);
+//       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+//       // await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
+//       res.status(200).json({
+//         status:"Success",
+//         accessToken,
+//       })
+
+//       return next();
+//     } catch (error: any) {
+//       return next(new ErrorHandler(error.message, 400));
+//     }
+//   }
+// );
+
+
+//modified code for error in  refresh token
 export const updateAccessToken = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -211,14 +270,15 @@ export const updateAccessToken = CatchAsyncError(
       if (!decoded) {
         return next(new ErrorHandler(message, 400));
       }
+
       const session = await redis.get(decoded.id as string);
-         
+
       if (!session) {
         return next(
           new ErrorHandler("Please login for access this resources!", 400)
         );
       }
-      
+
       const user = JSON.parse(session);
 
       const accessToken = jwt.sign(
@@ -239,21 +299,29 @@ export const updateAccessToken = CatchAsyncError(
 
       req.user = user;
 
+      // Set cookies for the new tokens
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
 
-      // await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
-      res.status(200).json({
-        status:"Success",
-        accessToken,
-      })
+      // Fixed the missing comma and closing parenthesis
+      await redis.set(user._id, JSON.stringify(user));
 
-      return next();
+      // Send the response
+      res.status(200).json({
+        status: "Success",
+        accessToken,
+      });
+
+      // Return after sending response to prevent further execution
+      return;
     } catch (error: any) {
+      // Send error response and return
       return next(new ErrorHandler(error.message, 400));
     }
   }
 );
+
+
 
 // get user info
 export const getUserInfo = CatchAsyncError(
